e40a3698a067fa1dfe41e00e59a7d12474b168a7
miqayelsrapionyan <miqayel@ucraft.com>
2023-08-21 18:42:46 +0400

    fix undefined array key value

--- src/Decorators/StyleDecorator.php
+++ src/Decorators/StyleDecorator.php
@@ -72,7 +72,7 @@ class StyleDecorator implements StyleDecoratorInterface
         $css .= !empty($this->styles) ? "{$this->selector} {" : '';
 
         foreach ($this->styles as $style) {
-            if ($style['value'] !== '0' && empty($style['value'])) {
+            if (isset($style['value']) && $style['value'] !== '0' && empty($style['value'])) {
                 continue;
             }
 
